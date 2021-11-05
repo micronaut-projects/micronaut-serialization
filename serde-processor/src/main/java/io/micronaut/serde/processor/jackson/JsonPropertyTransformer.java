c48975613c201bd0772146ce90f19c439f64a06b
graemerocher <graeme.rocher@gmail.com>
2021-11-05 10:54:45 +0100

    Change JsonProperty to a transformer

--- serde-processor/src/main/java/io/micronaut/serde/processor/jackson/JsonPropertyTransformer.java
+++ serde-processor/src/main/java/io/micronaut/serde/processor/jackson/JsonPropertyTransformer.java
@@ -22,41 +22,41 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.core.annotation.AnnotationValue;
 import io.micronaut.core.annotation.AnnotationValueBuilder;
 import io.micronaut.core.bind.annotation.Bindable;
-import io.micronaut.inject.annotation.TypedAnnotationMapper;
+import io.micronaut.inject.annotation.TypedAnnotationTransformer;
 import io.micronaut.inject.visitor.VisitorContext;
 import io.micronaut.serde.annotation.SerdeConfig;
 
 /**
  * Maps the {@link com.fasterxml.jackson.annotation.JsonProperty} annotation to {@link io.micronaut.serde.annotation.SerdeConfig}.
  */
-public class JsonPropertyMapper implements TypedAnnotationMapper<JsonProperty> {
+public class JsonPropertyTransformer implements TypedAnnotationTransformer<JsonProperty> {
     @Override
     public Class<JsonProperty> annotationType() {
         return JsonProperty.class;
     }
 
     @Override
-    public List<AnnotationValue<?>> map(AnnotationValue<JsonProperty> annotation, VisitorContext visitorContext) {
+    public List<AnnotationValue<?>> transform(AnnotationValue<JsonProperty> annotation, VisitorContext visitorContext) {
         final AnnotationValueBuilder<SerdeConfig> builder = AnnotationValue.builder(SerdeConfig.class);
         annotation.stringValue().ifPresent(s -> builder.member(SerdeConfig.PROPERTY, s));
         ArrayList<AnnotationValue<?>> values = new ArrayList<>();
 
         annotation.stringValue("defaultValue")
                 .ifPresent(s ->
-                    values.add(AnnotationValue.builder(Bindable.class)
-                                       .member("defaultValue", s).build())
+                                   values.add(AnnotationValue.builder(Bindable.class)
+                                                      .member("defaultValue", s).build())
                 );
         final JsonProperty.Access access = annotation.enumValue("access", JsonProperty.Access.class).orElse(null);
         if (access != null) {
             switch (access) {
-                case READ_ONLY:
-                    builder.member("readOnly", true);
+            case READ_ONLY:
+                builder.member("readOnly", true);
                 break;
-                case WRITE_ONLY:
-                    builder.member("writeOnly", true);
+            case WRITE_ONLY:
+                builder.member("writeOnly", true);
                 break;
-                default:
-                    // no-op
+            default:
+                // no-op
             }
         }
         values.add(builder.build());
