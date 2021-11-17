c1c20fa015c2aa37524518e65089e048d1527a70
graemerocher <graeme.rocher@gmail.com>
2021-11-17 10:39:53 +0100

    test static methods / getters

--- serde-jackson/src/test/groovy/io/micronaut/serde/jackson/annotation/JsonValueSpec.groovy
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/annotation/JsonValueSpec.groovy
@@ -57,4 +57,61 @@ class Test {
         cleanup:
         context.close()
     }
+
+    void "test json value on getter"() {
+        given:
+        def context = buildContext('''
+package jsonvalue;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.micronaut.serde.annotation.Serdeable;
+
+@Serdeable
+class Other {
+    private Test test;
+    public void setTest(jsonvalue.Test test) {
+        this.test = test;
+    }
+    public jsonvalue.Test getTest() {
+        return test;
+    }
+}
+@Serdeable
+class Test {
+    private final String name;
+    
+    private Test(String n) {
+        this.name = n;
+    }
+    @JsonValue
+    public String getName() {
+        return name;
+    }
+    
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    static Test valueOf(String name) {
+        return new Test(name);
+    }
+}
+''')
+        def o = newInstance(context, 'jsonvalue.Other')
+        def tClass = context.classLoader.loadClass('jsonvalue.Test')
+        o.test = tClass.valueOf("test")
+
+        when:
+        def result = writeJson(jsonMapper, o)
+
+        then:"JsonValue triggers toString()"
+        result == '{"test":"test"}'
+
+        when:
+        def read = jsonMapper.readValue(result, argumentOf(context, 'jsonvalue.Other'))
+
+        then:
+        read.test.name == 'test'
+
+        cleanup:
+        context.close()
+    }
 }
