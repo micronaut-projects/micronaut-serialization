93ae67a5f3fdc0e86d67e17a359c5ad7102569fc
Graeme Rocher <graeme.rocher@gmail.com>
2021-11-30 08:24:31 +0100

    fix kotlin syntax

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,7 +37,7 @@ val micronautVersion = providers.gradleProperty("micronautVersion")
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        maven { url "https://s01.oss.sonatype.org/content/repositories/snapshots/" }
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/") 
     }
     versionCatalogs {
         create("mn") {
