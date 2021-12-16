87ed5fbfa433580e315f070e10ea7d6f8cd2beaf
Denis Stepanov <denis.s.stepanov@oracle.com>
2021-12-16 15:37:50 +0700

    Improve tests

--- doc-examples/example-kotlin/src/test/kotlin/example/PlaceTest.kt
+++ doc-examples/example-kotlin/src/test/kotlin/example/PlaceTest.kt
@@ -9,10 +9,15 @@ import org.junit.jupiter.api.Test
 class PlaceTest {
     @Test
     fun testPlace(objectMapper: ObjectMapper) {
-        val result = objectMapper.writeValueAsString(Place(Point.valueOf(50, 100)))
+        val result = objectMapper.writeValueAsString(Place(Point.valueOf(50, 100), Point.valueOf(1, 2), Point.valueOf(3, 4)))
         val place = objectMapper.readValue(result, Place::class.java)
         Assertions.assertNotNull(place)
-        Assertions.assertEquals(100, place.point.coords()[0])
-        Assertions.assertEquals(50, place.point.coords()[1])
+        Assertions.assertEquals(50, place.point.coords()[0])
+        Assertions.assertEquals(100, place.point.coords()[1])
+        Assertions.assertEquals(2, place.pointCustomSer.coords()[0])
+        Assertions.assertEquals(1, place.pointCustomSer.coords()[1])
+        Assertions.assertEquals(4, place.pointCustomDes.coords()[0])
+        Assertions.assertEquals(3, place.pointCustomDes.coords()[1])
+        Assertions.assertEquals("""{"point":[100,50],"pointCustomSer":[2,1],"pointCustomDes":[3,4]}""", result)
     }
 }
