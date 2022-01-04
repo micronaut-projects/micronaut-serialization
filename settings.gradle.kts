750d1b599d4858336b153a2b6e33310d0c478815
graemerocher <graeme.rocher@gmail.com>
2022-01-04 18:24:42 +0100

    bump versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "4.2.6"
+    id("io.micronaut.build.shared.settings") version "5.1.0"
 }
 
 enableFeaturePreview("VERSION_CATALOGS")
