689f90c2a7dae0222975d0ea29a36fe2a345a94e
Graeme Rocher <graeme.rocher@oracle.com>
2022-07-21 12:57:34 +0200

    Upgrade to Micronaut 3.5 (#246)

--- serde-tck/build.gradle.kts
+++ serde-tck/build.gradle.kts
@@ -1,15 +1,18 @@
 plugins {
     id("groovy")
+    id("java-library")
 }
 dependencies {
     annotationProcessor(mn.micronaut.inject.java)
     annotationProcessor(projects.serdeProcessor)
     implementation(mn.micronaut.inject.java)
     implementation(projects.serdeApi)
+    implementation(projects.serdeSupport)
     implementation(projects.serdeProcessor)
     implementation(mn.micronaut.inject.java.test)
     compileOnly(mn.micronaut.inject.groovy)
     implementation(mn.micronaut.test.spock)
+    api(libs.jetbrains.annotations)
 
     if (!JavaVersion.current().isJava9Compatible()) {
         implementation(files(org.gradle.internal.jvm.Jvm.current().toolsJar))
