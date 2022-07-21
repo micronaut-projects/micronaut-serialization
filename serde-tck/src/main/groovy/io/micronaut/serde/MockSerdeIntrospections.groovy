1c3aa8085890d8ac295f7a150b81f81fa6fae232
Graeme Rocher <graeme.rocher@gmail.com>
2022-07-21 11:09:54 +0200

    fix failing tests

--- serde-tck/src/main/groovy/io/micronaut/serde/MockSerdeIntrospections.groovy
+++ serde-tck/src/main/groovy/io/micronaut/serde/MockSerdeIntrospections.groovy
@@ -5,6 +5,7 @@ import io.micronaut.context.annotation.Replaces
 import io.micronaut.core.annotation.NonNull
 import io.micronaut.core.beans.BeanIntrospection
 import io.micronaut.core.beans.BeanIntrospectionReference
+import io.micronaut.core.beans.BeanIntrospector
 import io.micronaut.core.naming.NameUtils
 import io.micronaut.core.reflect.InstantiationUtils
 import io.micronaut.core.reflect.ReflectionUtils
@@ -13,14 +14,70 @@ import io.micronaut.serde.support.DefaultSerdeIntrospections
 import jakarta.inject.Singleton
 
 import javax.tools.JavaFileObject
+import java.util.function.Predicate
+import java.util.stream.Collectors
 
 @Singleton
 @Replaces(DefaultSerdeIntrospections.class)
 class MockSerdeIntrospections extends DefaultSerdeIntrospections {
     private final ClassLoader classLoader
-
+    private List<BeanIntrospectionReference> references
     MockSerdeIntrospections(ApplicationContext context) {
         this.classLoader = context.getClassLoader()
+        if (!isInvalidClassLoader()) {
+            // horrible hack this
+            def f = ReflectionUtils.getRequiredField(classLoader.getClass(), "files")
+            f.setAccessible(true)
+            def files = f.get(classLoader)
+            def resolvedTypes = files.findAll({ JavaFileObject jfo ->
+                jfo.name.contains('$IntrospectionRef')
+            }).collect( { JavaFileObject jfo ->
+                String className = jfo.name.substring(14, jfo.name.length() - 6).replace('/', '.')
+                try {
+                    classLoader.loadClass(className)
+                } catch (ClassNotFoundException cnfe) {
+                    return null
+                }
+            })
+
+
+            references = resolvedTypes
+                    .findAll( { it != null })
+                    .collect() {
+                        (BeanIntrospectionReference) InstantiationUtils.instantiate(it)
+                    }
+        }
+    }
+
+    @Override
+    BeanIntrospector getBeanIntrospector() {
+        return new BeanIntrospector() {
+            @Override
+            Collection<BeanIntrospection<Object>> findIntrospections(@NonNull Predicate<? super BeanIntrospectionReference<?>> filter) {
+                return references.stream()
+                        .filter(filter)
+                        .filter(BeanIntrospectionReference::isPresent)
+                        .map(BeanIntrospectionReference::load)
+                        .collect(Collectors.toList()) + SHARED.findIntrospections(filter)
+            }
+
+            @Override
+            Collection<Class<?>> findIntrospectedTypes(@NonNull Predicate<? super BeanIntrospectionReference<?>> filter) {
+                return Collections.emptySet()
+            }
+
+            @Override
+            def <T> Optional<BeanIntrospection<T>> findIntrospection(@NonNull Class<T> beanType) {
+                def result = findIntrospections({ ref ->
+                    ref.isPresent() && ref.beanType == beanType
+                }).stream().findFirst()
+                if (result.isPresent()) {
+                    return result
+                } else {
+                    return SHARED.findIntrospection(beanType)
+                }
+            }
+        }
     }
 
     @Override
