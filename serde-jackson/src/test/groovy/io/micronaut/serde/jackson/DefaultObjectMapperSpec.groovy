2dd283e6d461c7f816f77f1b06281a2047b470af
Graeme Rocher <graeme.rocher@gmail.com>
2022-07-26 10:37:51 +0200

    Allow access to a default object mapper for cases where static access is required

---
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/DefaultObjectMapperSpec.groovy
@@ -0,0 +1,43 @@
+package io.micronaut.serde.jackson
+
+import io.micronaut.serde.ObjectMapper
+import io.micronaut.serde.annotation.Serdeable
+import spock.lang.Specification
+
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *
+ * @author graemerocher
+ */
+class DefaultObjectMapperSpec extends Specification {
+	
+    void "test default object mapper"() {
+        given:
+        ObjectMapper om = ObjectMapper.getDefault()
+
+        expect:
+        om.writeValueAsString(new Simple(name:"Fred")) == '{"name":"Fred"}'
+    }
+}
+
+@Serdeable
+class Simple {
+    String name
+}
+
+
