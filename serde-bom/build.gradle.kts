d34aadca39459761e7a7ad09cdfab8ed261931c8
Graeme Rocher <graeme.rocher@gmail.com>
2022-11-02 15:50:21 +0100

    fix bom check

--- serde-bom/build.gradle.kts
+++ serde-bom/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     id("io.micronaut.build.internal.bom")
 }
+
+micronautBom {
+    suppressions {
+        acceptedVersionRegressions.add("glassfish-jakarta-json")
+        acceptedLibraryRegressions.add("glassfish-jakarta-json")
+    }
+}
