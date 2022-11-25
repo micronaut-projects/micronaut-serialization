56421fb6774a21b843ecb7613fd1018e432ce1ef
Guillermo Calvo <guillermo@guillermo.dev>
2022-11-25 11:01:45 +0100

    Clean up build script

--- serde-gcp-cloudevents/build.gradle.kts
+++ serde-gcp-cloudevents/build.gradle.kts
@@ -2,28 +2,13 @@ plugins {
     id("io.micronaut.build.internal.module")
 }
 
-configurations.named("testRuntimeClasspath") {
-    resolutionStrategy.dependencySubstitution {
-        substitute(module("io.micronaut:micronaut-jackson-databind"))
-            .using(project(":serde-jackson")).because("we want to test Micronaut without jackson databind")
-    }
-}
-
 dependencies {
-    annotationProcessor(mn.micronaut.inject.java)
-    annotationProcessor(mn.micronaut.graal)
     annotationProcessor(projects.serdeProcessor)
-    annotationProcessor("io.micronaut.docs:micronaut-docs-asciidoc-config-props:2.0.0")
 
-	compileOnly(libs.managed.google.cloudevent.types)
-    api(projects.serdeApi)
+    implementation(projects.serdeSupport)
+    api(libs.managed.google.cloudevent.types)
 
-    testAnnotationProcessor(mn.micronaut.inject.java)
-    testAnnotationProcessor(projects.serdeProcessor)
-    testImplementation(mn.micronaut.inject.java.test)
-    testImplementation(mn.micronaut.test.spock)
-    testImplementation(libs.jetbrains.annotations)
-    testImplementation(libs.managed.google.cloudevent.types)
+    testImplementation(projects.serdeJackson)
 }
 
 micronautBuild {
