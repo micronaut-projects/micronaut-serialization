e2c55a4e6ed0405c9694422cf5071b73944ebe95
Tim Yates <tim.yates@gmail.com>
2022-12-01 15:44:55 +0000

    Remove pending feature from test that has been fixed

--- serde-jackson/src/test/groovy/io/micronaut/serde/jackson/object/ObjectSerdeSpec.groovy
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/object/ObjectSerdeSpec.groovy
@@ -1982,7 +1982,6 @@ class Test {
         writeJson(jsonMapper, beanUnderTest) == '{"foo":"bar"}'
     }
 
-    @PendingFeature(reason = 'Regression in Micronaut 4.0 when reading property values')
     void "optional nullable mix"() {
         given:
         def context = buildContext('example.Test', '''
