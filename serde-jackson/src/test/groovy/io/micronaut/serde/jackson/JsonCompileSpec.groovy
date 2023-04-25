fc6c59ee4400f5de50c7bae335f511366acb1ab1
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-25 15:35:31 +0200

    M2 (#431)
    * M2
    
    * Update test and reactor
    
    * Fix OOM
    
    ---------
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>
    Co-authored-by: Denis Stepanov <denis.s.stepanov@oracle.com>

--- serde-jackson/src/test/groovy/io/micronaut/serde/jackson/JsonCompileSpec.groovy
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/JsonCompileSpec.groovy
@@ -1,28 +1,16 @@
 package io.micronaut.serde.jackson
 
-import com.sun.source.util.JavacTask
+
+import io.micronaut.annotation.processing.JavaAnnotationMetadataBuilder
 import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
 import io.micronaut.annotation.processing.test.JavaParser
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.ApplicationContextBuilder
-import io.micronaut.core.annotation.NonNull
-import io.micronaut.core.beans.BeanIntrospection
-import io.micronaut.core.beans.BeanIntrospectionReference
-import io.micronaut.core.beans.BeanIntrospector
-import io.micronaut.core.reflect.InstantiationUtils
-import io.micronaut.core.reflect.ReflectionUtils
 import io.micronaut.core.type.Argument
 import io.micronaut.json.JsonMapper
 import io.micronaut.serde.MockSerdeIntrospections
-import io.micronaut.serde.config.annotation.SerdeConfig
-import io.micronaut.serde.support.DefaultSerdeIntrospections
 import io.micronaut.serde.ObjectMapper
-import io.micronaut.serde.SerdeIntrospections
 import org.intellij.lang.annotations.Language
-
-import javax.tools.JavaFileObject
-import java.util.function.Predicate
-import java.util.stream.Collectors
 
 class JsonCompileSpec extends AbstractTypeElementSpec implements JsonSpec {
 
@@ -32,32 +20,8 @@ class JsonCompileSpec extends AbstractTypeElementSpec implements JsonSpec {
 
     @Override
     protected JavaParser newJavaParser() {
-        return new JavaParser() {
-            @Override
-            JavacTask getJavacTask(JavaFileObject... sources) {
-                // TODO: remove horrible hack once micronaut-inject-java-test working in JDK 17
-                def lastTask = ReflectionUtils.findField(JavaParser, "lastTask")
-                        .get()
-                def diagnosticCollector = ReflectionUtils.findField(JavaParser, "diagnosticCollector")
-                        .get()
-                diagnosticCollector.setAccessible(true)
-                lastTask.setAccessible(true)
-                lastTask.set(
-                        this,
-                        (JavacTask) compiler.getTask(
-                                null, // explicitly use the default because old javac logs some output on stderr
-                                fileManager,
-                                diagnosticCollector.get(this),
-                                Collections.emptySet(),
-                                Collections.emptySet(),
-                                Arrays.asList(sources)
-                        )
-                )
-
-                return lastTask.get(this);
-
-            }
-        }
+        JavaAnnotationMetadataBuilder.clearCaches()
+        return super.newJavaParser()
     }
 
     ApplicationContext buildContext(String className, @Language("java") String source, Map<String, Object> properties) {
