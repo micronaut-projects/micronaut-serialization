8f023c128139bf166ed86eb18a713e5e6af3648d
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-09 10:36:25 +0200

    remove default methods
    Moved to core JsonMapper

--- serde-api/src/main/java/io/micronaut/serde/ObjectMapper.java
+++ serde-api/src/main/java/io/micronaut/serde/ObjectMapper.java
@@ -15,18 +15,11 @@
  */
 package io.micronaut.serde;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.Objects;
-
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.annotation.Nullable;
-import io.micronaut.core.type.Argument;
 import io.micronaut.json.JsonFeatures;
 import io.micronaut.json.JsonMapper;
+
+import java.util.Map;
 
 /**
  * Sub-interface of {@link JsonMapper} with customizations.
@@ -34,88 +27,9 @@ import io.micronaut.json.JsonMapper;
  * @author graemerocher
  */
 public interface ObjectMapper extends JsonMapper {
-    /**
-     * Read a value from the given input stream for the given type.
-     * @param inputStream The input stream
-     * @param type The type
-     * @param <T> The generic type
-     * @return The value or {@code null} if it decodes to null
-     * @throws IOException If an unrecoverable error occurs
-     */
-    default @Nullable <T> T readValue(@NonNull InputStream inputStream, @NonNull Class<T> type) throws IOException {
-        Objects.requireNonNull(type, "Type cannot be null");
-        return readValue(inputStream, Argument.of(type));
-    }
-
-    /**
-     * Read a value from the byte array for the given type.
-     * @param byteArray The byte array
-     * @param type The type
-     * @param <T> The generic type
-     * @return The value or {@code null} if it decodes to null
-     * @throws IOException If an unrecoverable error occurs
-     */
-    default @Nullable <T> T readValue(@NonNull byte[] byteArray, @NonNull Class<T> type) throws IOException {
-        Objects.requireNonNull(type, "Type cannot be null");
-        return readValue(byteArray, Argument.of(type));
-    }
-
-    /**
-     * Read a value from the given string for the given type.
-     * @param string The string
-     * @param type The type
-     * @param <T> The generic type
-     * @return The value or {@code null} if it decodes to null
-     * @throws IOException If an unrecoverable error occurs
-     */
-    default @Nullable <T> T readValue(@NonNull String string, @NonNull Class<T> type) throws IOException {
-        Objects.requireNonNull(type, "Type cannot be null");
-        return readValue(string, Argument.of(type));
-    }
-
-    /**
-     * Write the given value as a string.
-     * @param object The object
-     * @param <T> The generic type
-     * @return The string
-     * @throws IOException If an unrecoverable error occurs
-     */
-    @SuppressWarnings("unchecked")
-    default  @NonNull <T> String writeValueAsString(@NonNull T object) throws IOException {
-        Objects.requireNonNull(object, "Object cannot be null");
-        return writeValueAsString((Argument<T>) Argument.of(object.getClass()), object, StandardCharsets.UTF_8);
-    }
-
     @Override
     default JsonMapper cloneWithFeatures(JsonFeatures features) {
         return this;
-    }
-
-    /**
-     * Write the given value as a string.
-     * @param type The type, never {@code null}
-     * @param object The object
-     * @param <T> The generic type
-     * @return The string
-     * @throws IOException If an unrecoverable error occurs
-     */
-    default  @NonNull <T> String writeValueAsString(@NonNull Argument<T> type, @Nullable T object) throws IOException {
-        return writeValueAsString(type, object, StandardCharsets.UTF_8);
-    }
-
-    /**
-     * Write the given value as a string.
-     * @param type The type, never {@code null}
-     * @param object The object
-     * @param charset The charset, never {@code null}
-     * @param <T> The generic type
-     * @return The string
-     * @throws IOException If an unrecoverable error occurs
-     */
-    default  @NonNull <T> String writeValueAsString(@NonNull Argument<T> type, @Nullable T object, Charset charset) throws IOException {
-        Objects.requireNonNull(charset, "Charset cannot be null");
-        byte[] bytes = writeValueAsBytes(type, object);
-        return new String(bytes, charset);
     }
 
     /**
