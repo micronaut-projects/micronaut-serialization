f44a6b278e8df0e5e9e3a9a0d7033be57ac2245d
Graeme Rocher <graeme.rocher@oracle.com>
2023-09-01 09:02:00 +0200

    reduce error to warning

--- doc-examples/example-java/src/test/java/example/SerdeImportBuilderTest.java
+++ doc-examples/example-java/src/test/java/example/SerdeImportBuilderTest.java
@@ -1,23 +1,14 @@
 package example;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.List;
 
 import com.oracle.bmc.aidocument.model.AnalyzeDocumentResult;
-import com.oracle.bmc.aidocument.model.DocumentClassificationFeature;
-import com.oracle.bmc.aidocument.model.DocumentKeyValueExtractionFeature;
 import com.oracle.bmc.aidocument.model.DocumentMetadata;
 import com.oracle.bmc.aidocument.model.Page;
 import io.micronaut.serde.ObjectMapper;
-import io.micronaut.serde.PropertyFilter;
-import io.micronaut.serde.Serializer;
-import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
-import jakarta.inject.Singleton;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -41,15 +32,5 @@ class SerdeImportBuilderTest {
         Assertions.assertEquals(analyzeDocumentDetails, read);
         Assertions.assertEquals(analyzeDocumentDetails.getDocumentClassificationModelVersion(), read.getDocumentClassificationModelVersion());
 
-    }
-
-    @Singleton
-    @Named("explicitlySetFilter")
-    static class TestPropertyFilter implements PropertyFilter {
-
-        @Override
-        public boolean shouldInclude(Serializer.EncoderContext encoderContext, Serializer<Object> propertySerializer, Object bean, String propertyName, Object propertyValue) {
-            return true;
-        }
     }
 }
