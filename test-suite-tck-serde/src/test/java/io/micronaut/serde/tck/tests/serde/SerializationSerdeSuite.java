e492cc79c7b0fe680a5e3a6dc1adbbafbb018876
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-16 16:03:48 +0200

    add comment

--- test-suite-tck-serde/src/test/java/io/micronaut/serde/tck/tests/serde/SerializationSerdeSuite.java
+++ test-suite-tck-serde/src/test/java/io/micronaut/serde/tck/tests/serde/SerializationSerdeSuite.java
@@ -7,7 +7,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @ExcludeClassNamePatterns(
-    "io.micronaut.serde.tck.tests.bytebuffer.ByteBufferNativeTest"
+    "io.micronaut.serde.tck.tests.bytebuffer.ByteBufferNativeTest" // https://github.com/FasterXML/jackson-databind/blob/2.16/src/main/java/com/fasterxml/jackson/databind/ser/std/ByteBufferSerializer.java#L27-L35
 )
 @SelectPackages("io.micronaut.serde.tck.tests")
 @SuiteDisplayName("Serialization TCK Serde")
