17f862a686d07c5383fedc2039375e402d10a83d
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-12-11 17:36:08 +0100

    Fix deserializing of `@JsonValue`

--- serde-jackson-tck/src/main/java/io/micronaut/serde/jackson/jsonvalue/JdkVersion.java
+++ serde-jackson-tck/src/main/java/io/micronaut/serde/jackson/jsonvalue/JdkVersion.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micronaut.serde.tck.tests;
+package io.micronaut.serde.jackson.jsonvalue;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
