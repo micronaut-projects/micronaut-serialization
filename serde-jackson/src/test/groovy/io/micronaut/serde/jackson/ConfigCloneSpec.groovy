7cc8708db44a519a7d5c738cc5d2f6ea1f5256be
yawkat <jonas.konrad@oracle.com>
2024-01-08 12:08:11 +0100

    deser test

--- serde-jackson/src/test/groovy/io/micronaut/serde/jackson/ConfigCloneSpec.groovy
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/ConfigCloneSpec.groovy
@@ -6,9 +6,11 @@ import io.micronaut.context.annotation.ConfigurationProperties
 import io.micronaut.core.bind.annotation.Bindable
 import io.micronaut.serde.ObjectMapper
 import io.micronaut.serde.annotation.Serdeable
+import io.micronaut.serde.config.DeserializationConfiguration
 import io.micronaut.serde.config.SerdeConfiguration
 import io.micronaut.serde.config.SerializationConfiguration
 import io.micronaut.serde.config.annotation.SerdeConfig
+import io.micronaut.serde.exceptions.SerdeException
 import spock.lang.Specification
 
 class ConfigCloneSpec extends Specification {
@@ -16,13 +18,30 @@ class ConfigCloneSpec extends Specification {
         given:
         def ctx = ApplicationContext.run()
         def original = ctx.getBean(ObjectMapper)
-        def modified = original.cloneWithConfiguration(ctx.getBean(OciSerdeConfiguration), ctx.getBean(OciSerializationConfiguration), null)
+        def modified = original.cloneWithConfiguration(ctx.getBean(OciSerdeConfiguration), ctx.getBean(OciSerializationConfiguration), ctx.getBean(OciDeserializationConfiguration))
         def testBean = new TestBean(empty: [], bytes: new byte[] {1, 2})
 
         expect:
         original.writeValueAsString(testBean) == '{"bytes":[1,2]}'
         modified.writeValueAsString(testBean) == '{"empty":[],"bytes":"AQI="}'
         original.writeValueAsString(testBean) == '{"bytes":[1,2]}' // double-check the old one wasn't broken
+
+        original.writeValueAsString(testBean) == '{"bytes":[1,2]}'
+        modified.writeValueAsString(testBean) == '{"empty":[],"bytes":"AQI="}'
+        original.writeValueAsString(testBean) == '{"bytes":[1,2]}' // double-check the old one wasn't broken
+
+        when:
+        original.readValue('{"missing":"foo"}', TestBean)
+        then:
+        noExceptionThrown()
+        when:
+        modified.readValue('{"missing":"foo"}', TestBean)
+        then:
+        thrown SerdeException
+        when:
+        original.readValue('{"missing":"foo"}', TestBean)
+        then:
+        noExceptionThrown()
 
         cleanup:
         ctx.close()
@@ -48,5 +67,14 @@ class ConfigCloneSpec extends Specification {
         @Bindable(defaultValue = "NON_NULL")
         @Override
         SerdeConfig.SerInclude getInclusion();
+    }
+
+    // micronaut-oraclecloud doesn't do this but we need some deser test
+    @ConfigurationProperties("oci.serde.deserialization")
+    @Bean(typed = OciDeserializationConfiguration.class)
+    static interface OciDeserializationConfiguration extends DeserializationConfiguration {
+        @Bindable(defaultValue = "false")
+        @Override
+        boolean isIgnoreUnknown()
     }
 }
