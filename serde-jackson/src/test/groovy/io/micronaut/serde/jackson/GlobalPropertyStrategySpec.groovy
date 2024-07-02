425696c939401fc74b1ccf89df120793f37e85fb
Alexei <alexeimyshkouski@gmail.com>
2024-07-03 01:30:32 +0300

    add tests for @JsonProperty-annotated fields

--- serde-jackson/src/test/groovy/io/micronaut/serde/jackson/GlobalPropertyStrategySpec.groovy
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/GlobalPropertyStrategySpec.groovy
@@ -48,6 +48,27 @@ class GlobalPropertyStrategySpec extends Specification {
         ctx.close()
     }
 
+    def 'test global property strategy SNAKE_CASE with JsonProperty annotated fields'() {
+        given:
+        def ctx = ApplicationContext.run(['micronaut.serde.property-naming-strategy': "SNAKE_CASE"])
+        def mapper = ctx.getBean(JsonMapper)
+
+        when:
+        def val = mapper.writeValueAsString(new MyBeanWithExplicitlyNamedProperties("hello", 123))
+        then:
+        val == '{"explicit_foo_bar_prop_name":"hello","abc_xyz":123}'
+
+        when:
+        def bean = mapper.readValue(val, MyBeanWithExplicitlyNamedProperties)
+
+        then:
+        bean.fooBar() == "hello"
+        bean.abcXyz() == 123
+
+        cleanup:
+        ctx.close()
+    }
+
     def 'test global property strategy LOWER_DOT_CASE'() {
         given:
         def ctx = ApplicationContext.run(['micronaut.serde.property-naming-strategy': "LOWER_DOT_CASE"])
