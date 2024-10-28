3845f32d0a30327a3f1dcb7dd9c30924346414e8
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-28 10:42:21 +0100

    use regex patterns

--- test-suite-http-server-tck-netty/src/test/java/io/micronaut/serde/http/server/tck/netty/NettyHttpServerTestSuite.java
+++ test-suite-http-server-tck-netty/src/test/java/io/micronaut/serde/http/server/tck/netty/NettyHttpServerTestSuite.java
@@ -22,13 +22,11 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @ExcludeClassNamePatterns({
-        "io.micronaut.http.server.tck.tests.jsonview.JsonViewTest",
-        "io.micronaut.http.server.tck.tests.hateoas.JsonErrorTest",
-        "io.micronaut.http.server.tck.tests.hateoas.VndErrorTest",
-        "io.micronaut.http.server.tck.tests.hateoas.JsonErrorSerdeTest",
-    "io.micronaut.http.server.tck.tests.constraintshandler.ControllerConstraintHandlerTest",
-    "io.micronaut.http.server.tck.tests.forms.FormsInputNumberOptionalTest",
-    "io.micronaut.http.server.tck.tests.forms.FormsSubmissionsWithListsTest",
+        "^io\\.micronaut\\.http\\.server\\.tck\\.tests\\.jsonview\\.JsonViewsTest$",
+        "^io\\.micronaut\\.http\\.server\\.tck\\.tests\\.hateoas\\.JsonErrorTest$",
+        "^io\\.micronaut\\.http\\.server\\.tck\\.tests\\.hateoas\\.VndErrorTest$",
+        "^io\\.micronaut\\.http\\.server\\.tck\\.tests\\.hateoas\\.JsonErrorSerdeTest$",
+        "^io\\.micronaut\\.http\\.server\\.tck\\.tests\\.forms\\.FormsInputNumberOptionalTest$"
 })
 @SelectPackages({
         "io.micronaut.http.server.tck.tests",
