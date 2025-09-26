5624c7373477305a4c80306fc5b2cbae94075aba
radovanradic <radicr@gmail.com>
2025-09-26 11:51:32 +0200

    Record deserialization fails when there are custom constructors

--- serde-jackson/src/test/groovy/io/micronaut/serde/jackson/JacksonBasicSerdeSpec.groovy
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/JacksonBasicSerdeSpec.groovy
@@ -1,5 +1,6 @@
 package io.micronaut.serde.jackson
 
+import io.micronaut.context.annotation.Property
 import io.micronaut.core.type.Argument
 import io.micronaut.json.JsonMapper
 import io.micronaut.serde.AbstractBasicSerdeSpec
@@ -7,7 +8,10 @@ import io.micronaut.serde.ObjectWithArrayRecordNotNull
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 
+import java.time.Instant
+
 @MicronautTest
+@Property(name = "micronaut.serde.deserialization.ignore-unknown", value = "false")
 class JacksonBasicSerdeSpec extends AbstractBasicSerdeSpec {
 
     @Inject
@@ -23,4 +27,12 @@ class JacksonBasicSerdeSpec extends AbstractBasicSerdeSpec {
             obj.vals() == []
     }
 
+    def "record with multiple constructors"() {
+        when:
+        def myRecord = new MyRecord("value", 10, Date.from(Instant.now().minusSeconds(60)))
+        def str = jsonMapper.writeValueAsString(myRecord)
+        def result = jsonMapper.readValue(str, MyRecord)
+        then:
+        myRecord == result
+    }
 }
---
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/MyRecord.java
@@ -0,0 +1,26 @@
+package io.micronaut.serde.jackson;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.serde.annotation.Serdeable;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Serdeable
+record MyRecord(String fooBar, int abcXyz, @Nullable Date theDate, @Nullable String otherStr) {
+
+    MyRecord {
+        if (theDate == null) {
+            theDate = Date.from(Instant.now());
+        }
+    }
+
+    public MyRecord(String fooBar, int abcXyz, Date theDate) {
+        this(fooBar, abcXyz, theDate, "random");
+    }
+
+    public MyRecord(String fooBar, int abcXyz) {
+        this(fooBar, abcXyz, Date.from(Instant.now()), "random");
+    }
+}
