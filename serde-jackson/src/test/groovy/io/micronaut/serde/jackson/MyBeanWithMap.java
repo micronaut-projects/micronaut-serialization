5f7e80e518ac5efd008fa6b6ff01d894f93389ba
Denis Stepanov <denis.s.stepanov@oracle.com>
2025-09-29 11:22:47 +0200

    Add missing `@JsonAnySetter`

--- serde-jackson/src/test/groovy/io/micronaut/serde/jackson/MyBeanWithMap.java
+++ serde-jackson/src/test/groovy/io/micronaut/serde/jackson/MyBeanWithMap.java
@@ -1,6 +1,7 @@
 package io.micronaut.serde.jackson;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
@@ -11,6 +12,6 @@ import java.util.Map;
 record MyBeanWithMap(String fooBar, int abcXyz, @Nullable @JsonInclude(JsonInclude.Include.NON_EMPTY) MyNestedBeanWithMap nested) {
 
     @Serdeable
-    record MyNestedBeanWithMap(Long id, @Nullable @JsonAnyGetter Map<String, Object> map) {
+    record MyNestedBeanWithMap(Long id, @Nullable @JsonAnyGetter @JsonAnySetter Map<String, Object> map) {
     }
 }
