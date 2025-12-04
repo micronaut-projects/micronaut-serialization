71a32dd82a4ce4b7e0eb0a1d491bd798a151ee46
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-04 10:52:34 +0100

    Unignore DatabindJsonFilterSpec
    with jackson 3 this probably needs https://github.com/micronaut-projects/micronaut-core/pull/12239

--- test-suite-tck-jackson-databind/src/test/groovy/io/micronaut/serde/tck/jackson/databind/DatabindJsonFilterSpec.groovy
+++ test-suite-tck-jackson-databind/src/test/groovy/io/micronaut/serde/tck/jackson/databind/DatabindJsonFilterSpec.groovy
@@ -2,9 +2,7 @@ package io.micronaut.serde.tck.jackson.databind
 
 import io.micronaut.context.ApplicationContextBuilder
 import io.micronaut.serde.jackson.JsonFilterSpec
-import spock.lang.Ignore
 
-@Ignore("Fails in MN5")
 class DatabindJsonFilterSpec extends JsonFilterSpec {
 
     @Override
