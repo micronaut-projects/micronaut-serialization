865be488d82ce7dc154009c32702975da6cd9956
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-04 18:49:44 +0200

    Revert "tck: ignore jackson Class Level JSON Format Propagation"
    This reverts commit 5e0677c7c64ba85ebfa7149acabf10fbab4ccc2e.

--- test-suite-tck-jackson-databind/src/test/groovy/io/micronaut/serde/tck/jackson/databind/DatabindJsonFormatSpec.groovy
+++ test-suite-tck-jackson-databind/src/test/groovy/io/micronaut/serde/tck/jackson/databind/DatabindJsonFormatSpec.groovy
@@ -13,11 +13,6 @@ import java.util.Locale
 
 class DatabindJsonFormatSpec extends JsonFormatSpec {
 
-    @Override
-    protected boolean supportsClassLevelJsonFormatPropagation() {
-        false
-    }
-
     @PendingFeature(reason = "Remove CompoundLocaleObjectMapperListener when Jackson Databind handles compound @JsonFormat(locale) values")
     void "plain Jackson Databind supports compound JsonFormat locale values"() {
         given:
