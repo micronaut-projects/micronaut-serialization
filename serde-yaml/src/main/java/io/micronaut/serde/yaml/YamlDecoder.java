e462e24d1a21e16928fc4bfc3c8757efd444478f
Mousrij Hamza <hamza.mousrij@oracle.com>
2026-07-12 14:49:27 +0100

    Fix bug: setting the UTF8 as StandardCharsets.

--- serde-yaml/src/main/java/io/micronaut/serde/yaml/YamlDecoder.java
+++ serde-yaml/src/main/java/io/micronaut/serde/yaml/YamlDecoder.java
@@ -29,8 +29,11 @@ import org.snakeyaml.engine.v2.schema.CoreSchema;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
@@ -69,10 +72,10 @@ public class YamlDecoder extends AbstractStreamDecoder {
         emptyStringAsNull = yamlConfiguration.isEmptyStringAsNull();
         LoadSettings loadSettings = LoadSettings.builder()
             .setSchema(new CoreSchema())
-            .setParseComments(true)
             .build();
         resolver = loadSettings.getSchema().getScalarResolver();
-        Iterator<Event> events = new Parse(loadSettings).parseInputStream(inputStream).iterator();
+        Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        Iterator<Event> events = new Parse(loadSettings).parseReader(reader).iterator();
         while (events.hasNext()) {
             Event nextEvent = events.next();
             if (nextEvent.getEventId() == Event.ID.DocumentStart) {
